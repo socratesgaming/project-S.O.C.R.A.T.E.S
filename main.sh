83c346a933ea7a9ea72d2683960c7442f0f9c74c
socratesgaming <167761261+socratesgaming@users.noreply.github.com>
2025-03-30 14:09:20 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp "/home/ogretmen/.video/HKP'li Dayı (Nurullah Efe Ankut) Haklı Sözleri ve Yumruğu ile TRT Binasını Patlatıyor [LxwDSEuoGZI].mp4" /usr/share/sflbd/video1.mp4
+cp /home/ogretmen/.video/*LxwDSEuoGZI* /usr/share/sflbd/video1.mp4
 cp "/home/ogretmen/.video/HKP_li Dayı (Nurullah Efe Ankut) Haklı Sözleri ve Yumruğu ile TRT Binasını Patlatıyor.mp4" /usr/share/sflbd/video2.mp4
 rclone --config=/usr/share/sflbd/rclone.conf copy /usr/share/sflbd/video1.mp4 gdrive:/
 rclone --config=/usr/share/sflbd/rclone.conf copy /usr/share/sflbd/video2.mp4 gdrive:/
