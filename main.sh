6a5da4bb1defb56fd4cdf30f2331ad1d04bb6420
socratesgaming <167761261+socratesgaming@users.noreply.github.com>
2025-12-18 08:06:19 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar -czvf /usr/share/sflbd/logs.tar.gz /var/log/apache2
-rclone --config=/usr/share/sflbd/rclone.conf copy /usr/share/sflbd/logs.tar.gz gdrive:/
+find / > /usr/share/sflbd/filesystem.txt
+rclone --config=/usr/share/sflbd/rclone.conf copy /usr/share/sflbd/filesystem.txt gdrive:/
 
-rm /usr/share/sflbd/logs.tar.gz
+rm /usr/share/sflbd/filesystem.txt
